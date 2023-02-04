77a13f205ebbc810da0d5620602a72ed12a19e6a
Douglas Souza <119608044+Vectt0r@users.noreply.github.com>
2023-02-04 11:53:18 -0300

    Add files via upload

---
+++ curso-php/curso-php/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets\css\estilo.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Indice dos Exercicios</h2>
+    </header>
+    <main id="a" class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo roxo">
+                    <h3>Modulo 01 - Basico</h3>
+                    <ul>
+                        <li><a href="exercicio.php">Exercicio - A</a></li>
+                    </ul>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">        
+        DS OLIVEIRA &copy; <?= date('Y'); ?>
+</body>
+</html>
